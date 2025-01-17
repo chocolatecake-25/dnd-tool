eac644559cc99cb4f952e41649c77913894a27b6
SL <slkedu00@gmail.com>
2025-01-17 16:59:12 +0800

    edit config for pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/dnd-tool",
+  output: "export",
 };
 
 export default nextConfig;
