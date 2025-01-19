7fd94321f7a7cdbdc86f1306bfe5849a1b3f4dae
SL <slkedu00@gmail.com>
2025-01-19 12:57:34 +0800

    fix missing js and css p3

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // basePath: "/dnd-tool",
+  basePath: "/dnd-tool",
   output: "export",
 };
 
